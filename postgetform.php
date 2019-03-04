46eb08f52dcfd4a893aec4f2888e0b8723935b34
AdelaVodvarkova <vod.adela@gmail.com>
2019-03-04 11:52:06 +0100

    Ukol 6 - formular - dodelany

---
+++ postgetform.php
@@ -0,0 +1,16 @@
+
+<form method="POST">
+ Jméno:    <input type="text"name="firstName"><br>
+  Heslo:   <input type="password"name="secret"><br>
+Souhlasím    <input type="checkbox"name="optIn"<br>
+  <br>  <input type="submit">
+
+</form>
+
+<?php
+var_dump($_POST);
+?>
+
+
+
+
