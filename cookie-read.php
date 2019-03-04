1fffc01c29b387791fe7766bf3c1cb44ae8b67fb
AdelaVodvarkova <vod.adela@gmail.com>
2019-03-04 11:24:01 +0100

    Ukol 5 - cookies - dodelany

---
+++ cookie-read.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AdelaVodvarkova
+ * Date: 2019-02-28
+ * Time: 20:30
+ */
+
+if(isset($_COOKIE['accessed']))
+ {echo $_COOKIE['accessed'];}
+else{echo "not accessed";}
+?>
+
---
+++ cookie-set.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AdelaVodvarkova
+ * Date: 2019-02-28
+ * Time: 20:33
+ */
+setcookie('accessed', '4.3.2019', time()+60*60);
+var_dump($_COOKIE);
